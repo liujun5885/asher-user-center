3abefbb1b1d34da5e53aa6a439aea86a1dfc39ee
junliu <junliu@appannie.com>
2019-11-28 22:38:45 +0800

    update

--- sqitch/ugc/deploy/auth_user.sql
+++ sqitch/ugc/deploy/auth_user.sql
@@ -3,15 +3,17 @@
 BEGIN;
 
 CREATE TABLE user_basic (
-    id serial PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     region VARCHAR(8) NOT NULL,
     phonenumber BIGINT UNIQUE NOT NULL,
-    username character varying(100) NOT NULL,
-    email character varying(100) NOT NULL,
-    password character varying(128) NOT NULL,
+    username VARCHAR(128) NOT NULL,
+    email  VARCHAR(128) NOT NULL,
+    password VARCHAR(128) NOT NULL,
     is_active boolean NOT NULL,
     created_at TIMESTAMP without time zone default (now() at time zone 'utc'),
     updated_at TIMESTAMP without time zone default (now() at time zone 'utc')
 );
+
+ALTER SEQUENCE user_basic_id_seq RESTART WITH 1000000;
 
 COMMIT;
